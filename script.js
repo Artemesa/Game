b0263e60b0537ee54aa74efed602efceb96bd618
Daniel <daniel.j.sotirov@gmail.com>
2023-02-17 12:30:17 +0200

    mestene

--- script.js
+++ script.js
@@ -5,14 +5,31 @@ let ny = 3;
 let sqside = 50;
 let herox = 2;
 let heroy = 1;
+let heroimg = new Image();
+heroimg.src = "hero.png";
 canvas.width = nx * sqside;
 canvas.height = ny * sqside;
-for ( let i = 0; i < nx; i++) {
-    for (let j = 0; j < ny; j++) {
-        context.strokeRect(i * sqside, j * sqside, sqside, sqside);
+
+//context.fillRect(herox * sqside, heroy * sqside, sqside, sqside);
+function drawMap() {
+    //context.fillStyle = "white";
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(heroimg, herox * sqside, heroy * sqside, sqside, sqside);
+    for ( let i = 0; i < nx; i++) {
+        for (let j = 0; j < ny; j++) {
+            context.strokeRect(i * sqside, j * sqside, sqside,  sqside);
+        }
     }
 }
-//context.fillRect(herox * sqside, heroy * sqside, sqside, sqside);
-let heroimg = new image();
-img.src = "hero.png"
-context.drawimage(heroimg, herox * sqside, heroy * sqside, sqside, sqside);
+
+drawMap();
+
+canvas.onclick = function(e) {
+    let x = e.x - canvas.offsetLeft;
+    let y = e.y - canvas.offsetTop;
+    herox = Math.floor( x / sqside);
+    heroy = Math.floor(y / sqside);
+    drawMap();
+    //console.log(canvas);
+    //console.log(x + " " + y);
+}
